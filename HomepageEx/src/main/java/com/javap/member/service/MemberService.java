00d373eac1efafef87762d54845ab60308ce8e63
YoungsuPark <nice2seeu86@gmail.com>
2016-02-08 17:59:24 +0900

    MemberService.java 파일 생성

---
+++ HomepageEx/src/main/java/com/javap/member/service/MemberService.java
@@ -0,0 +1,19 @@
+package com.javap.member.service;
+
+import java.util.Map;
+
+public interface MemberService {
+	
+	
+	void insertMember(Map<String, Object> map) throws Exception;
+	
+	Map<String, Object> loginMember(Map<String, Object> map) throws Exception;
+	
+	Map<String, Object> memberInfo(Map<String, Object> map) throws Exception;
+
+	void updateMember(Map<String, Object> map) throws Exception;
+
+	void deleteMember(Map<String, Object> map) throws Exception;
+
+	
+}
