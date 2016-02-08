d75f040e030ba37eea0e5e3e8aa99bade2fd83e7
YoungsuPark <nice2seeu86@gmail.com>
2016-02-08 17:41:59 +0900

    web.xml 파일 생성

---
+++ HomepageEx/src/main/java/com/javap/common/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.javap.common.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.javap.common.common.CommandMap;
+
+@Controller
+public class HomeController {
+	Logger log = Logger.getLogger(this.getClass());
+	
+	@RequestMapping(value="/home/welcomeHome.do")
+	public ModelAndView welcomeHome(CommandMap commandMap){
+		ModelAndView mv = new ModelAndView("/home/welcomeHome");
+		return mv;
+	}
+	
+	
+}
