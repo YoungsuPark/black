28d7d04c9af971df155616840ab87311503806fd
YoungsuPark <nice2seeu86@gmail.com>
2016-02-12 15:25:49 +0900

    selectoBoardList Type 변경 : List Type -> Map Type

--- HomepageEx/src/main/java/com/javap/memoboard/service/MemoBoardService.java
+++ HomepageEx/src/main/java/com/javap/memoboard/service/MemoBoardService.java
@@ -1,11 +1,10 @@
 package com.javap.memoboard.service;
 
-import java.util.List;
 import java.util.Map;
 
 public interface MemoBoardService {
 
-	List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception;
+	Map<String, Object> selectBoardList(Map<String, Object> map) throws Exception;
 	Map<String, Object> selectMemoBoardDetail(Map<String, Object> map) throws Exception;
 	void insertMemoBoard(Map<String, Object> map) throws Exception;
 	void updateMemoBoard(Map<String, Object> map) throws Exception;
