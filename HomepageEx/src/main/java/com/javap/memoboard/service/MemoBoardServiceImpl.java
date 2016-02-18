67c909b938583469b309d372cfe4aa473919e323
YoungsuPark <nice2seeu86@gmail.com>
2016-02-18 15:29:31 +0900

    패이징 처리 부분을 공통 부분으로 이동 후 파일 유틸에 대한 로직 추가

--- HomepageEx/src/main/java/com/javap/memoboard/service/MemoBoardServiceImpl.java
+++ HomepageEx/src/main/java/com/javap/memoboard/service/MemoBoardServiceImpl.java
@@ -1,68 +1,50 @@
 package com.javap.memoboard.service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
+import com.javap.common.utils.FileUtil;
+import com.javap.common.utils.PaginationUtil;
 import com.javap.memoboard.dao.MemoBoardDAO;
 
 @Service("mbService")
 public class MemoBoardServiceImpl implements MemoBoardService {
 	Logger log = Logger.getLogger(this.getClass());
 	
+	@Resource(name="fileUtil")
+	private FileUtil fileUtil;
+	
+	@Resource(name="paginationUtil")
+	private PaginationUtil paginationUtil;
+	
 	@Resource(name="mbDAO")
 	private MemoBoardDAO mbDAO;
 	
 	@Override
-	public Map<String, Object> selectBoardList(Map<String, Object> map) throws Exception {
-		int currentPage = 1; // 현재 페이지
-		int recordPerPage = 10; // 한 페이지당 보여줄 record 수
-		int pagesPerGroup = 5; // 한 그룹당 보여줄 page 수	
-		int numOfRecords = mbDAO.numOfRecords(); // 총 record 수
-		int numOfPages = (int)(numOfRecords / recordPerPage + (numOfRecords % recordPerPage == 0? 0:1)); // 총 페이지 수
-		
-		if(map.get("page") != null){
-			currentPage = Integer.parseInt((String)(map.get("page")));
-			if(currentPage < 1){
-				currentPage = 1;
-			} else if(currentPage >= numOfPages){
-				currentPage = numOfPages;
-			}
-		}
-		int startNumPerPage = recordPerPage * (currentPage - 1); // 한 페이지 당 게시물의 스타트 번호 
-		int groupNum = (int)(currentPage / pagesPerGroup + (currentPage % pagesPerGroup == 0? 0:1)); // 현재 그룹
-		
-		int endPageNumPerGroup = groupNum * pagesPerGroup; // 한 그룹당 끝 페이지 번호
-		int startPageNumPerGroup = endPageNumPerGroup - (pagesPerGroup - 1);
-		if(endPageNumPerGroup >= numOfPages){
-			endPageNumPerGroup = numOfPages;
-		}
-		map.put("startNumPerPage", startNumPerPage);
-		map.put("recordPerPage", recordPerPage);
-		
-		List<Map<String, Object>> list = mbDAO.selectBoardList(map);	
-		Map<String, Object> resultMap = new HashMap<String, Object>();
+	public Map<String, Object> selectBoardList(Map<String, Object> map) throws Exception {	
+		Map<String, Object> resultMap = paginationUtil.pagination(map);
+		List<Map<String, Object>> list = mbDAO.selectBoardList(map);
 		resultMap.put("list", list);
-		resultMap.put("currentPage", currentPage);
-		resultMap.put("pagesPerGroup", pagesPerGroup);
-		resultMap.put("startPageNumPerGroup", startPageNumPerGroup);
-		resultMap.put("endPageNumPerGroup", endPageNumPerGroup);
-	
 		return resultMap;
 	}
 	
 	@Override
-	public void insertMemoBoard(Map<String, Object> map) throws Exception {
+	public void insertMemoBoard(Map<String, Object> map, HttpServletRequest request) throws Exception {
 		mbDAO.insertMemoBoard(map);
+		
 		if(map != null){
 			map.put("IDX", (int)map.get("idx"));
 			mbDAO.updateFamliy(map);
 		}
+		List<Map<String, Object>> list = fileUtil.InsertFileInfo(map, request);
+		for(int i=0, size=list.size(); i<size; i++)
+			mbDAO.inserFile(list.get(i));
 	}
 	
 	@Override
@@ -78,16 +60,11 @@ public class MemoBoardServiceImpl implements MemoBoardService {
 	}
 	
 	@Override
-	public Map<String, Object> deleteMemoBoard(Map<String, Object> map) throws Exception {
-		Map<String, Object> resultMap = new HashMap<>();
-		int checkReply = mbDAO.checkReply(map);
-		if(checkReply == 0){
-			mbDAO.deleteMemoBoard(map);	
-			resultMap.put("deleteMessage", "삭제되었습니다.");
-		} else {
-			resultMap.put("deleteMessage", "댓글이 달린 글은 삭제 할 수 없습니다.");
-		}
-		return resultMap;
+	public void deleteMemoBoard(Map<String, Object> map) throws Exception {
+		if(mbDAO.checkReply(map) == 0)
+			mbDAO.deleteMemoBoard(map);
+		 else 
+			mbDAO.deleteParaentMemoBoard(map);
 	}
 	
 	@Override
