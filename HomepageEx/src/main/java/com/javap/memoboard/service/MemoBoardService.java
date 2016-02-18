495c6824b7afd30d89e35c56cb0eb0f914f46678
YoungsuPark <nice2seeu86@gmail.com>
2016-02-18 15:29:09 +0900

    delete 서비스 로직 타입 변경

--- HomepageEx/src/main/java/com/javap/memoboard/service/MemoBoardService.java
+++ HomepageEx/src/main/java/com/javap/memoboard/service/MemoBoardService.java
@@ -2,13 +2,15 @@ package com.javap.memoboard.service;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface MemoBoardService {
 
 	Map<String, Object> selectBoardList(Map<String, Object> map) throws Exception;
 	Map<String, Object> selectMemoBoardDetail(Map<String, Object> map) throws Exception;
-	void insertMemoBoard(Map<String, Object> map) throws Exception;
+	void insertMemoBoard(Map<String, Object> map, HttpServletRequest request) throws Exception;
 	void updateMemoBoard(Map<String, Object> map) throws Exception;
-	Map<String, Object> deleteMemoBoard(Map<String, Object> map)throws Exception;
+	void deleteMemoBoard(Map<String, Object> map)throws Exception;
 	void insetReply(Map<String, Object> map)throws Exception;
 
 }
