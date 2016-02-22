0ad8d6272ff6842afa7bb8b081a6f8143841fa76
YoungsuPark <nice2seeu86@gmail.com>
2016-02-22 18:24:08 +0900

    none

--- HomepageEx/src/main/java/com/javap/member/service/MemberService.java
+++ HomepageEx/src/main/java/com/javap/member/service/MemberService.java
@@ -4,14 +4,8 @@ import java.util.Map;
 
 public interface MemberService {
 	
-	
 	void insertMember(Map<String, Object> map) throws Exception;
-	
 	Map<String, Object> memberInfo(Map<String, Object> map) throws Exception;
-
-	void updateMember(Map<String, Object> map) throws Exception;
-
-	void deleteMember(Map<String, Object> map) throws Exception;
-
-	
+	Map<String, Object> updateMember(Map<String, Object> map) throws Exception;
+	void deleteMember(Map<String, Object> map) throws Exception;	
 }
