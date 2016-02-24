6397097b0a0f15c38a8a199c8ab4227a3282f35e
YoungsuPark <nice2seeu86@gmail.com>
2016-02-24 17:03:59 +0900

    javadoc

--- HomepageEx/src/main/java/com/javap/home/service/HomeServiceImpl.java
+++ HomepageEx/src/main/java/com/javap/home/service/HomeServiceImpl.java
@@ -7,21 +7,24 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.javap.home.dao.HomeDAO;
 
 @Service("homeService")
+@Transactional
 public class HomeServiceImpl implements HomeService {
 	Logger log = Logger.getLogger(this.getClass());
 	
 	@Resource(name="homeDAO")
 	private HomeDAO homeDAO;
-	
+
+	/**
+	 * 로그인
+	 */
 	@Override
-	public Map<String, Object> login(Map<String, Object> map, HttpSession session) throws Exception {
-		log.debug("login info : " + map);
-		Map<String, Object> user = homeDAO.login(map);
-		log.debug("user info : " + user);
+	public Map<String, Object> login(Map<String, Object> map, HttpSession session) throws Exception {	
+		Map<String, Object> user = homeDAO.login(map);	
 		if(user != null){
 			session.setAttribute("sessionId", user.get("ID"));
 			session.setAttribute("sessionIdx", user.get("IDX"));
