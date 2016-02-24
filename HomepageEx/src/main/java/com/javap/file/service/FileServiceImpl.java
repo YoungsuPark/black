56d8a39bc6c7ca558719de7223b46d8d6ee75e5c
YoungsuPark <nice2seeu86@gmail.com>
2016-02-24 17:02:42 +0900

    javadoc

--- HomepageEx/src/main/java/com/javap/file/service/FileServiceImpl.java
+++ HomepageEx/src/main/java/com/javap/file/service/FileServiceImpl.java
@@ -6,16 +6,21 @@ import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.javap.file.dao.FileDAO;
 
 @Service("fileService")
+@Transactional
 public class FileServiceImpl implements FileService {
 	Logger log = Logger.getLogger(this.getClass());
 	
 	@Resource(name="fileDAO")
 	private FileDAO fileDAO;
 	
+	/**
+	 * DB에 파일 정보 가져오기
+	 */
 	@Override
 	public Map<String, Object> selectFileInfo(Map<String, Object> map) throws Exception {
 		return fileDAO.selectFileInfo(map);
